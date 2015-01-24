6439287a391d95f36325deb23c8d5f88cf81d134
Matthew Hicks <mkigikm@gmail.com>
2015-01-24 16:46:03 -0500

    adds solve method

--- maze_solver.rb
+++ maze_solver.rb
@@ -3,7 +3,8 @@ class Maze
     "*" => :wall,
     " " => :free,
     "S" => :start,
-    "E" => :finish
+    "E" => :finish,
+    "#" => :solution
   }
 
   def load_file(filename)
@@ -14,18 +15,30 @@ class Maze
         FILE_TO_SYMBOLS[square]
       end
     end
+
+    self
   end
 
   def [](pos)
     @maze_board[pos.first][pos.last]
   end
 
+  def []=(pos, square)
+    @maze_board[pos.first][pos.last] = square
+  end
+
   def dimensions
     [@maze_board.count, @maze_board.first.count]
   end
 
-  def initialize(filename)
-    load_file(filename)
+  def self.from_file(filename)
+    Maze.new.load_file(filename)
+  end
+
+  def initialize(board=nil)
+    unless board.nil?
+      @maze_board = board.map { |row| row.dup }
+    end
   end
 
   def to_s
@@ -36,6 +49,10 @@ class Maze
     end.join("\n")
   end
 
+  def inspect
+    "<Maze:#{__id__} @maze_board:#{@maze_board.__id__}>"
+  end
+
   def start
     find_symbol(:start)
   end
@@ -44,16 +61,79 @@ class Maze
     find_symbol(:finish)
   end
 
+  def solve
+    dup.solve!
+  end
+
+  def solve!
+    start_pos = start
+    passage_queue = [start_pos]
+    came_from = {start_pos => nil}
+
+    until passage_queue.empty?
+      current_passage = passage_queue.shift
+
+      break if self[current_passage] == :finish
+
+      free_neighbors(current_passage).each do |neighbor|
+        unless came_from.keys.include?(neighbor)
+          passage_queue << neighbor
+          came_from[neighbor] = current_passage
+        end
+      end
+    end
+
+    path = find_path(came_from)
+    path.each do |pos|
+      unless self[pos] == :start || self[pos] == :finish
+        self[pos] = :solution
+      end
+    end
+
+    self
+  end
+
+  def dup
+    Maze.new(@maze_board)
+  end
+
   private
   def find_symbol(sym)
     rows, cols = dimensions
     rows.times do |row|
       cols.times do |col|
-        return [row, col] if maze[[row, col]] == sym
+        return [row, col] if self[[row, col]] == sym
       end
     end
 
     nil
   end
 
+  def free_neighbors(pos)
+    neighbors(pos).reject { |square| self[square] == :wall }
+  end
+
+  def neighbors(pos)
+    [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0]
+    ].map do |delta|
+      new_pos = [pos.first + delta.first, pos.last + delta.last]
+    end
+  end
+
+  def find_path(came_from)
+    finish_pos = finish
+    return nil unless came_from.keys.include?(finish_pos)
+
+    current_pos = finish_pos
+    [].tap do |path|
+      until self[current_pos] == :start
+        path << current_pos
+        current_pos = came_from[current_pos]
+      end
+    end
+  end
 end
